69ce99b45f6c4b70030ca88ba27974f3cfe91777
EvansCoder <wangaevans84@gmail.com>
2022-06-30 13:40:52 +0300

    fixing the loader

--- pages/_app.js
+++ pages/_app.js
@@ -10,18 +10,6 @@ import {
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth);
-  // useEffect(() => {
-  //   if (user) {
-  //     db.collection("users").doc(user.uid).set(
-  //       {
-  //         email: user.email,
-  //         lastSeen: serverTimestamp,
-  //         photoURL: user.photoURL,
-  //       },
-  //       { merge: true }
-  //     );
-  //   }
-  // }, [user]);
 
   if (loading) return <Loading />;
   if (!user) return <Login />;
